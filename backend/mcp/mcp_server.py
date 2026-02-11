b3d2c1ed4a2256f05d4e7c6a131d8a618021c22d
Pramod Vadiraja <pramod@ordio.com>
2026-02-12 00:37:52 +0100

    chore: update packages

--- backend/mcp/mcp_server.py
+++ backend/mcp/mcp_server.py
@@ -1,9 +1,12 @@
 import asyncio
 
 from fastmcp import FastMCP
+import uvicorn
+from fastapi import FastAPI
 from utils.configs import MCP_PORT, QDRANT_HOST, QDRANT_PORT, USE_CLOUD
 from utils.factory import get_embedding_model
 from utils.qdrant_service import QdrantService
+from contextlib import asynccontextmanager
 
 mcp = FastMCP("MCP_PersonalData")
 qdrant_service = QdrantService(
@@ -22,8 +25,32 @@ async def search_data(query: str):
     return "\n\n".join([doc.payload["text"] for doc in data])
 
 
+mcp_app = mcp.http_app("/mcp/")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Yield to the MCP lifespan manager
+    async with mcp_app.lifespan(app):
+        yield
+
+
+mcp = FastMCP(name="Test MCP server", log_level="DEBUG")
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/health_check")
+def health_check():
+    return {"status": "OK from MCP server"}
+
+
+app.mount("/", mcp_app)
+
+
 # can run inspector to explore the tools using mcp dev mcp_server.py
 if __name__ == "__main__":
     # mcp.run(transport="stdio")
     # mcp.run(transport="sse", host="127.0.0.1", port=9000, path="/sse")
-    asyncio.run(mcp.run_streamable_http_async(host="0.0.0.0", port=MCP_PORT))
+    uvicorn.run(app, host="0.0.0.0", port=MCP_PORT)
